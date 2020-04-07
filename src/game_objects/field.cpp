a49e6140a63c24595c2920518e879bfa299e95a3
BraydonKains <kainsbraydon@gmail.com>
2020-04-06 22:36:56 -0400

    Seg faultgit add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .!

--- src/game_objects/field.cpp
+++ src/game_objects/field.cpp
@@ -7,9 +7,18 @@ Field::Field(int _width, int _height) {
 }
 
 Field::~Field() {
+	SDL_FreeSurface(field_surface);
 }
 
 RenderData Field::getRenderData() {
-	RenderData x;
-	return x;
+	SDL_Rect src;
+	src.x = 0;
+	src.y = 0;
+	src.w = 200;
+	src.h = 400;
+
+	RenderData render_data;
+	render_data.surface = field_surface;
+	render_data.src_rect = &src;
+	return render_data;
 }
--- src/screens/start_screen.cpp
+++ src/screens/start_screen.cpp
@@ -34,8 +34,18 @@ void StartScreen::render() {
 	SDL_RenderClear(renderer);
 
 	RenderData field_render = field->getRenderData();
-	
-	/* SDL_RenderCopy(renderer, field_texture, NULL, NULL); */
+	SDL_Texture* field_texture = SDL_CreateTextureFromSurface(renderer, field_render.surface);
+	if(field_texture == NULL) {
+		SDL_Log("SDL_CreateTexture() failed: %s", SDL_GetError());
+		exit(1);
+	}
+	SDL_Rect* dest_rect;
+	dest_rect->x = 1140;
+	dest_rect->y = 100;
+	dest_rect->w = 200;
+	dest_rect->h = 400;
+
+	SDL_RenderCopy(renderer, field_texture, field_render.src_rect, dest_rect);
 
 	SDL_RenderPresent(renderer);
 }
